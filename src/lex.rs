a5f89798f336cb8cd352e98a08f0703833b8fcef
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-07-11 08:25:30 -0300

    feat: implements peek function in a Lexer to show the next token without modifing the current iterator

--- src/lex.rs
+++ src/lex.rs
@@ -9,6 +9,22 @@ pub mod parse;
 
 type Lexer<'a> = logos::Lexer<'a, Token>;
 
+pub trait PeekLexer<'a> {
+    /// Peek at the next token in the lexer. This provides a way to look at the next token without consuming it.
+    /// 
+    /// Returns a new lexer where the token is consumed and the next token if it exists
+    fn peek(&self) -> (Lexer<'a>, Option<Result<Token>>);
+}
+
+impl<'lexer> PeekLexer<'lexer> for Lexer<'lexer> {
+    fn peek(&self) -> (Lexer<'lexer>, Option<Result<Token>>) {
+        let mut peeker = self.clone();
+        let next = peeker.next().map(|r| r.map_err(|_| Unknown.into())); 
+        
+        (peeker, next)
+    }
+}
+
 /// Take the next token from the lexer and check if it is the expected token
 pub fn take_exact(mut lexer: Lexer, token: Token) -> Result<Lexer> {
     match lexer.next() {
