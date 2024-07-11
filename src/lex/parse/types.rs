b241558241b10a33b98c0079b1132e00fbb88cc1
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-07-10 22:28:16 -0300

    feat: separates compound type parsing, also allowing trailing commas

--- src/lex/parse/types.rs
+++ src/lex/parse/types.rs
@@ -2,38 +2,22 @@ use anyhow::{Context, Result};
 
 use crate::lex::{ast::{Identifier, TypeExpression}, token::Token, Lexer, error::ParseError::*};
 
+/// Parses a type expression
+/// 
+/// Syntax:
+///     TypeIdentifier
+///     ( TypeExpression [, TypeExpression]* )
 pub fn parse_type(mut lexer: Lexer) -> Result<(Lexer, TypeExpression)> {
     let init = lexer.span().end;
 
     match lexer.next() {
         Some(Ok(Token::TypeIdentifier(t))) => Ok((lexer, TypeExpression::Identifier(Identifier::Type(t)))),
         Some(Ok(Token::LParen)) => {
-            let mut compound = vec![];
-            let init = lexer.span().start;            
-            
-            loop {
-                let (l, te) = {
-                    let src = lexer.source()[init..lexer.span().end].trim();
-                    parse_type(lexer).with_context(|| format!("parsing compound type expression `{}`", src))
-                }?;
-
-                lexer = l;
-                compound.push(te);
-
-                match lexer.next() {
-                    Some(Ok(Token::RParen)) => return Ok((lexer, TypeExpression::Compound(compound))),
-                    Some(Ok(Token::Comma)) => continue,
-                    Some(Ok(token)) => return Err(UnexpectedToken { 
-                        token, 
-                        slice: lexer.slice().into(), 
-                        span: lexer.span(), 
-                        expected: "`,` or `)`".to_string() 
-                    }).with_context(|| format!("parsing compound type expression `{}`", lexer.source()[init..lexer.span().end].trim())),
-                    Some(Err(_)) => bail!(Unknown),
-                    None => bail!(UnexpectedEOF),
-                }
-            }
-        }
+            let src = lexer.source()[init..lexer.span().end].trim();
+            parse_compound_type(lexer)
+            .map(|(l, te)| (l, TypeExpression::Compound(te)))
+            .with_context(|| format!("parsing compound type expression at `{}`", src))
+        },
         Some(Ok(token)) => return Err(UnexpectedToken { 
             token, 
             slice: lexer.slice().into(), 
@@ -42,5 +26,44 @@ pub fn parse_type(mut lexer: Lexer) -> Result<(Lexer, TypeExpression)> {
         }).with_context(|| format!("parsing type expression `{}`", lexer.source()[init..lexer.span().end].trim())),
         Some(Err(_)) => bail!(Unknown),
         None => bail!(UnexpectedEOF),
+    }
+}
+
+pub fn parse_compound_type(mut lexer: Lexer) -> Result<(Lexer, Vec<TypeExpression>)> {
+    let mut compound = vec![];
+    let init = lexer.span().start;            
+    
+    loop {
+        let mut peeker = lexer.clone();
+
+        // Check if the next token is a right parenthesis
+        // If so, return the compound type expression
+        if let Some(Ok(Token::RParen)) = peeker.next() {
+            return Ok((peeker, compound))
+        }
+
+        let (l, te) = {
+            let src = lexer.source()[init..lexer.span().end].trim();
+            parse_type(lexer).with_context(|| format!("parsing compound type expression `{}`", src))?
+        };
+
+        lexer = l;
+        compound.push(te);
+
+        match lexer.next() {
+            // End of compound type expession
+            Some(Ok(Token::RParen)) => return Ok((lexer, compound)), 
+            // Next field in compound type expression
+            Some(Ok(Token::Comma)) => continue,
+            // Unexpected token
+            Some(Ok(token)) => return Err(UnexpectedToken { 
+                token, 
+                slice: lexer.slice().into(), 
+                span: lexer.span(), 
+                expected: "`,` or `)`".to_string() 
+            }).with_context(|| format!("parsing compound type expression `{}`", lexer.source()[init..lexer.span().end].trim())),
+            Some(Err(_)) => bail!(Unknown),
+            None => bail!(UnexpectedEOF),
+        }
     }
 }
